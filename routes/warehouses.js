ffe059702bbcf44a4e4e7d9251edb72060f6183d
Justin Muir <justinmuir@gmail.com>
2022-07-16 16:17:29 -0400

    edit warehouse complete, fixed bug in pagkage

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -53,4 +53,53 @@ router.post('/', (req, res) => {
     res.status(201).send(warehouseToAdd);
 });
 
+// PUT edit warehouse
+router.post('/:warehouseId', (req, res) => {
+    
+    const { name, address, city, country, contact } = req.body;
+    
+    try {
+        if(!validatePhone(contact.phone)) {
+            throw "Phone number format is invalid. Numbers should be +1 (XXX) XXX-XXXX"
+        }
+
+        if (!validateEmail(contact.email)) {
+            throw "Email number format is invalid."
+        }
+
+        if (!name || !address || !city || !country || !contact || !contact.name || !contact.position || !contact.phone || !contact.email) {
+            throw "All fields must be filled in."
+        }
+
+        if (warehouseData.findIndex(wh => wh.id === req.params.warehouseId) === -1) {
+           throw "That warehouse ID does not exist." 
+        }
+    } catch (err) {
+        res.status(400).send(err);
+    }
+    
+    const warehouseUpdate = {
+        "id": req.params.warehouseId,
+        "name": name,
+        "address": address,
+        "city": city,
+        "country": country,
+        "contact": {
+            "name": contact.name,
+            "position": contact.position,
+            "phone": contact.phone,
+            "email": contact.email
+        }
+    }
+    
+    const warehouseIndex = warehouseData.findIndex(wh => wh.id === req.params.warehouseId);
+    warehouseData[warehouseIndex] = warehouseUpdate;
+    
+    fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
+        if (err) throw err;
+    });
+
+    res.status(201).send(warehouseUpdate);
+});
+
 module.exports = router;
