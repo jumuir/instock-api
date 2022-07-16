bd39e378672137297d0fe89e6c572e6aee502f1c
dmytrotrydub <tridub.d87@gmail.com>
2022-07-16 18:15:04 -0400

    Blocker Delete Warehouse

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,9 +1,42 @@
-const express = require('express');
-const fs = require('fs');
-const warehouseData = require('../data/warehouses.json');
-const inventoryData = require('../data/inventories.json');
+const express = require("express");
+const fs = require("fs");
+const warehouseData = require("../data/warehouses.json");
+const warehouseTest = require("../data/wareHouseTest.json");
+const inventoryData = require("../data/inventories.json");
+const inventoryTest = require("../data/inventories_copy.json");
 const router = express.Router();
 
+router.delete("/:warehouseId", (req, res) => {
+  // console.log(req.params.warehouseId);
+  if (req.params) {
+    console.log("Request sucessful");
+  }
+  //? deleting a warehouse
+  const newWarehouseList = warehouseTest.filter(
+    (warehouse) => warehouse.id !== req.params.warehouseId
+  );
 
+  console.log(newWarehouseList);
+
+  fs.writeFile("./data/wareHouseTest.json", JSON.stringify(newWarehouseList), (err) => {
+    // console.log(err);
+  });
+
+  // // ? deleting corresponding inventory, that belongs to deleted warehouse
+
+  // const newInventory = inventoryTest.filter((inv) => inv.warehouseID !== req.params.warehouseId);
+  // console.log(newInventory);
+
+  // fs.writeFile("./data/inventories_copy.json", JSON.stringify(newInventory), (err) => {
+  //   // throw new Error(err);
+  // });
+
+  // const warehouseInvData = {
+  //   newWarehouseList,
+  //   // newInventory,
+  // };
+
+  res.send(newWarehouseList);
+});
 
 module.exports = router;
