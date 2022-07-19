5e823554095ba801b12c1bbf52861116cd480768
dmytrotrydub <tridub.d87@gmail.com>
2022-07-18 20:15:20 -0400

    Created logic for delete warehouses End-point

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,42 +1,46 @@
 const express = require("express");
 const fs = require("fs");
 const warehouseData = require("../data/warehouses.json");
-const warehouseTest = require("../data/wareHouseTest.json");
 const inventoryData = require("../data/inventories.json");
-const inventoryTest = require("../data/inventories_copy.json");
 const router = express.Router();
 
 router.delete("/:warehouseId", (req, res) => {
-  // console.log(req.params.warehouseId);
   if (req.params) {
     console.log("Request sucessful");
   }
   //? deleting a warehouse
-  const newWarehouseList = warehouseTest.filter(
-    (warehouse) => warehouse.id !== req.params.warehouseId
+
+  const warehouseToDelete = warehouseData.findIndex(
+    (warehouse) => warehouse.id === req.params.warehouseId
   );
-
-  console.log(newWarehouseList);
-
-  fs.writeFile("./data/wareHouseTest.json", JSON.stringify(newWarehouseList), (err) => {
-    // console.log(err);
+  warehouseData.splice(warehouseToDelete, 1);
+  // ? writing a new list of Warehouses to .json list
+  fs.writeFile("./data/warehouses.json", JSON.stringify(warehouseData), (err) => {
+    if (err) {
+      console.log(err);
+    }
   });
 
-  // // ? deleting corresponding inventory, that belongs to deleted warehouse
+  // ? deleting corresponding inventory, that belongs to deleted warehouse
 
-  // const newInventory = inventoryTest.filter((inv) => inv.warehouseID !== req.params.warehouseId);
-  // console.log(newInventory);
+  for (let i = inventoryData.length - 1; i >= 0; i--) {
+    if (inventoryData[i].warehouseID === req.params.warehouseId) {
+      inventoryData.splice(i, 1);
+    }
+  }
+  // ? writing a new list of Warehouses to .json list
+  fs.writeFile("./data/inventories.json", JSON.stringify(inventoryData), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 
-  // fs.writeFile("./data/inventories_copy.json", JSON.stringify(newInventory), (err) => {
-  //   // throw new Error(err);
-  // });
+  const warehouseInvData = {
+    warehouseData,
+    inventoryData,
+  };
 
-  // const warehouseInvData = {
-  //   newWarehouseList,
-  //   // newInventory,
-  // };
-
-  res.send(newWarehouseList);
+  res.send(warehouseInvData).status(200);
 });
 
 module.exports = router;
