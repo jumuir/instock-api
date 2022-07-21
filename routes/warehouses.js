5fb50ab00c99de0406b7d65615be922bbd9092aa
tonyliaccount@github.com <tony.li.email@gmail.com>
2022-07-20 20:38:14 -0700

    Attempt linter fix #7

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -8,171 +8,171 @@ const { validatePhone, validateEmail } = require('../middleware/validators');
 
 // GET List of All warehouses
 router.get('/', function (req, res) {
-  res.status(201).json(warehouseData);
+    res.status(201).json(warehouseData);
 });
 
 // GET single warehouse and inventory list
 router.get("/:id", function (req, res) {
-    
-    const paramsID = req.params.id
-    const singleWarehouse = warehouseData.find(warehouse => warehouse.id === paramsID)
+        
+        const paramsID = req.params.id
+        const singleWarehouse = warehouseData.find(warehouse => warehouse.id === paramsID)
 
-    if (singleWarehouse === undefined){
-        res.status(400).send("Warehouse does not exist")
-    }  else {
-        const warehouseInventory = inventoryData.filter(inventory => inventory.warehouseID === singleWarehouse.id)
-        const warehouseInformation = {...singleWarehouse, warehouseInventory}
-        res.status(200).send(warehouseInformation)
-    }   
+        if (singleWarehouse === undefined){
+                res.status(400).send("Warehouse does not exist")
+        }    else {
+                const warehouseInventory = inventoryData.filter(inventory => inventory.warehouseID === singleWarehouse.id)
+                const warehouseInformation = {...singleWarehouse, warehouseInventory}
+                res.status(200).send(warehouseInformation)
+        }     
 })
 
 // POST new warehouse
 router.post('/', (req, res) => {
-  const { name, address, city, country, contact } = req.body;
-  try {
-    if (!validatePhone(contact.phone)) {
-      throw "Phone number format is invalid. Numbers should be +1 (XXX) XXX-XXXX";
+    const { name, address, city, country, contact } = req.body;
+    try {
+        if (!validatePhone(contact.phone)) {
+            throw "Phone number format is invalid. Numbers should be +1 (XXX) XXX-XXXX";
+        }
+
+        if (!validateEmail(contact.email)) {
+            throw "Email format is invalid.";
+        }
+
+        if (
+            !name ||
+            !address ||
+            !city ||
+            !country ||
+            !contact ||
+            !contact.name ||
+            !contact.position ||
+            !contact.phone ||
+            !contact.email
+        ) {
+            throw "All fields must be filled in.";
+        }
+    } catch (err) {
+        res.status(400).send(err);
+        return;
     }
 
-    if (!validateEmail(contact.email)) {
-      throw "Email format is invalid.";
-    }
+    const warehouseToAdd = {
+        id: uuid(),
+        name: name,
+        address: address,
+        city: city,
+        country: country,
+        contact: {
+            name: contact.name,
+            position: contact.position,
+            phone: contact.phone,
+            email: contact.email,
+        },
+    };
 
-    if (
-      !name ||
-      !address ||
-      !city ||
-      !country ||
-      !contact ||
-      !contact.name ||
-      !contact.position ||
-      !contact.phone ||
-      !contact.email
-    ) {
-      throw "All fields must be filled in.";
-    }
-  } catch (err) {
-    res.status(400).send(err);
-    return;
-  }
+    warehouseData.push(warehouseToAdd);
 
-  const warehouseToAdd = {
-    id: uuid(),
-    name: name,
-    address: address,
-    city: city,
-    country: country,
-    contact: {
-      name: contact.name,
-      position: contact.position,
-      phone: contact.phone,
-      email: contact.email,
-    },
-  };
+    fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
+        if (err) throw err;
+    });
 
-  warehouseData.push(warehouseToAdd);
-
-  fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
-    if (err) throw err;
-  });
-
-  res.status(201).send(warehouseToAdd);
+    res.status(201).send(warehouseToAdd);
 });
 
 // PUT edit warehouse
 router.put('/:warehouseId', (req, res) => {
-  const { name, address, city, country, contact } = req.body;
+    const { name, address, city, country, contact } = req.body;
 
-  try {
-    if (!validatePhone(contact.phone)) {
-      throw "Phone number format is invalid. Numbers should be +1 (XXX) XXX-XXXX";
+    try {
+        if (!validatePhone(contact.phone)) {
+            throw "Phone number format is invalid. Numbers should be +1 (XXX) XXX-XXXX";
+        }
+
+        if (!validateEmail(contact.email)) {
+            throw "Email format is invalid.";
+        }
+
+        if (
+            !name ||
+            !address ||
+            !city ||
+            !country ||
+            !contact ||
+            !contact.name ||
+            !contact.position ||
+            !contact.phone ||
+            !contact.email
+        ) {
+            throw "All fields must be filled in.";
+        }
+
+        if (warehouseData.findIndex((wh) => wh.id === req.params.warehouseId) === -1) {
+            throw "That warehouse ID does not exist.";
+        }
+    } catch (err) {
+        res.status(400).send(err);
+        return;
     }
 
-    if (!validateEmail(contact.email)) {
-      throw "Email format is invalid.";
-    }
+    const warehouseUpdate = {
+        id: req.params.warehouseId,
+        name: name,
+        address: address,
+        city: city,
+        country: country,
+        contact: {
+            name: contact.name,
+            position: contact.position,
+            phone: contact.phone,
+            email: contact.email,
+        },
+    };
 
-    if (
-      !name ||
-      !address ||
-      !city ||
-      !country ||
-      !contact ||
-      !contact.name ||
-      !contact.position ||
-      !contact.phone ||
-      !contact.email
-    ) {
-      throw "All fields must be filled in.";
-    }
+    const warehouseIndex = warehouseData.findIndex((wh) => wh.id === req.params.warehouseId);
+    warehouseData[warehouseIndex] = warehouseUpdate;
 
-    if (warehouseData.findIndex((wh) => wh.id === req.params.warehouseId) === -1) {
-      throw "That warehouse ID does not exist.";
-    }
-  } catch (err) {
-    res.status(400).send(err);
-    return;
-  }
+    fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
+        if (err) throw err;
+    });
 
-  const warehouseUpdate = {
-    id: req.params.warehouseId,
-    name: name,
-    address: address,
-    city: city,
-    country: country,
-    contact: {
-      name: contact.name,
-      position: contact.position,
-      phone: contact.phone,
-      email: contact.email,
-    },
-  };
-
-  const warehouseIndex = warehouseData.findIndex((wh) => wh.id === req.params.warehouseId);
-  warehouseData[warehouseIndex] = warehouseUpdate;
-
-  fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
-    if (err) throw err;
-  });
-
-  res.send(warehouseUpdate);
+    res.send(warehouseUpdate);
 });
 
 router.delete('/:warehouseId', (req, res) => {
-  if (req.params) {
-    console.log("Request sucessful");
-  }
-  //? deleting a warehouse
+    if (req.params) {
+        console.log("Request sucessful");
+    }
+    //? deleting a warehouse
 
-  const warehouseToDelete = warehouseData.findIndex(
-    (warehouse) => warehouse.id === req.params.warehouseId
-  );
-  warehouseData.splice(warehouseToDelete, 1);
+    const warehouseToDelete = warehouseData.findIndex(
+        (warehouse) => warehouse.id === req.params.warehouseId
+    );
+    warehouseData.splice(warehouseToDelete, 1);
 
-  const deletedWarehouse = warehouseData.at(warehouseToDelete);
-    // ? writing a new list of Warehouses to .json list
-      fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
-        if (err) {
-          console.log(err);
+    const deletedWarehouse = warehouseData.at(warehouseToDelete);
+        // ? writing a new list of Warehouses to .json list
+            fs.writeFile('./data/warehouses.json', JSON.stringify(warehouseData), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+    // ? deleting corresponding inventory, that belongs to deleted warehouse
+    const deletedInventory = [];
+    for (let i = inventoryData.length - 1; i >= 0; i--) {
+        if (inventoryData[i].warehouseID === req.params.warehouseId) {
+            deletedInventory.push(inventoryData[i]);
+            inventoryData.splice(i, 1);
         }
-      });
-  // ? deleting corresponding inventory, that belongs to deleted warehouse
-  const deletedInventory = [];
-  for (let i = inventoryData.length - 1; i >= 0; i--) {
-    if (inventoryData[i].warehouseID === req.params.warehouseId) {
-      deletedInventory.push(inventoryData[i]);
-      inventoryData.splice(i, 1);
     }
-  }
 
-  // ? writing a new list of Warehouses to .json list
-  fs.writeFile('./data/inventories.json', JSON.stringify(inventoryData), (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-  const deletedData = { deletedWarehouse, deletedInventory };
-  res.send(deletedData).status(200);
+    // ? writing a new list of Warehouses to .json list
+    fs.writeFile('./data/inventories.json', JSON.stringify(inventoryData), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    const deletedData = { deletedWarehouse, deletedInventory };
+    res.send(deletedData).status(200);
 });
 
 
